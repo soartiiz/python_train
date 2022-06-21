4d7de34fdac5d97c8000ffd5902988abd864bbbf
Lucas <lucas.folliot@gmail.com>
2022-06-21 22:41:52 +0200

    wip sql connect

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from flask import Flask, request, jsonify
+from asyncio.windows_events import NULL
+from flask import Flask, request, jsonify, g
 from helpers import isInt
+import sqlite3
 
 app = Flask(__name__)
 app.run(debug = True)
+
+DATABASE = 'database.db'
+
+def get_db():
+  db = getattr(g, '_database', None)
+  if db is None:
+      db = g._database = sqlite3.connect(DATABASE)
+  return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+with app.app_context():
+  cur = get_db().cursor()
+  cur.execute('''CREATE TABLE IF NOT EXISTS "pokemons" (
+              "id"	INTEGER NOT NULL UNIQUE,
+              "name"	TEXT NOT NULL,
+              "description"	TEXT NOT NULL,
+              PRIMARY KEY("id" AUTOINCREMENT)
+            );''')
 
 pokemons = [
   { "id": 0, "name": 'Salamèche' },
@@ -12,29 +37,50 @@ pokemons = [
 
 @app.route("/", methods = ['GET'])
 def findPokemon():
-  return jsonify(pokemons)
+  with app.app_context():
+    cur = get_db().cursor()
 
-@app.route("/add", methods = ['POST'])
-def addPokemon():
-  data = request.get_json()
+    cur.execute('SELECT * FROM pokemons')
+    pokemons = cur.fetchall()
 
-  if not "name" in data:
-    return 'Name required', 400
-
-  data['id'] = len(pokemons)
-  pokemons.append(data)
-  return jsonify(pokemons)
+    close_connection('')
+    return jsonify(pokemons)
 
 @app.route("/<id>", methods = ['GET'])
 def findOnePokemon(id):
   if isInt(id) != True:
     return isInt(id)
 
-  try:
-    pokemon = next(item for item in pokemons if item["id"] == int(id))
-    return pokemon
-  except StopIteration:
-    return 'Not found', 404
+  with app.app_context():
+    cur = get_db().cursor()
+    cur.execute('SELECT * FROM pokemons WHERE id=' + id)
+    pokemon = cur.fetchone()
+
+    if not pokemon:
+      return 'Not found', 404
+
+    close_connection('')
+  return jsonify(pokemon)
+
+@app.route("/create", methods = ['POST'])
+def createPokemon():
+  data = request.get_json()
+
+  if not "name" in data:
+    return 'Name required', 400
+
+  if not "description" in data:
+    return 'Description required', 400
+
+  with app.app_context(): 
+    cur = get_db().cursor()
+    cur.execute("INSERT INTO pokemons (name, description) VALUES ('" + data['name'] + "', '" + data['description'] + "')")
+    get_db().commit()
+    close_connection('')
+
+  data['id'] = len(pokemons)
+  pokemons.append(data)
+  return jsonify(pokemons)
 
 @app.route("/<id>", methods = ['PUT'])
 def updatePokemon(id):
