ed9e533f7083edd4e9594e1de0915791dcded01c
Lucas <lucas.folliot@gmail.com>
2022-06-23 00:15:00 +0200

    fix style

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import { Pokemon } from './pokemon'
 import { PokemonDTO } from './dto/pokemon'
 
-// const app = document.querySelector<HTMLDivElement>('#app')!
+const mainContainer = document.getElementById('main-container')
 
 const handlePokemons = async (): Promise<PokemonDTO[]> => {
   const request = await fetch('http://localhost:5000', { method: 'GET' })
@@ -10,9 +10,6 @@ const handlePokemons = async (): Promise<PokemonDTO[]> => {
 }
 
 const pokemons = await handlePokemons()
-console.log(pokemons)
-
-const mainContainer = document.getElementById('main-container')
 
 pokemons.forEach((p: PokemonDTO) => {
   const pokemon = new Pokemon(p.name, p.description)
